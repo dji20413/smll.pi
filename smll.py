d319125fb69a06966045952e73c540c9314a35f9
dji20413 <dji20413@gmail.com>
2013-08-03 02:22:49 +0900

    multi file support

--- smll.py
+++ smll.py
@@ -3,7 +3,6 @@
 # smll.pi-0.1.0
 # smll client for raspberry pi
 
-
 import os 
 import sys
 import RPi.GPIO as GPIO
@@ -14,9 +13,9 @@ import logging
 import datetime as dt
 
 # logging.basicConfig(filename='main.py.log',level=logging.DEBUG)
-last_ctx_updated = dt.datetime.now() 
-ctx_interval = 5 
-play_index = 0
+last_ctx_updated = 0 
+ctx_interval = 60 
+track = 0
 
 def print_no_endl(str) :
 	sys.stdout.write(str)
@@ -32,38 +31,40 @@ def ctx_update() :
 	global ctx_interval 
 
 	now = dt.datetime.now()
-	elapsed = (now-last_ctx_updated).seconds 
 
-	print "elapsed: %(el)d" % {'el' : elapsed}
+	elapsed = 0
+	if last_ctx_updated != 0 :
+		elapsed = (now-last_ctx_updated).seconds 
+		print "elapsed: %(el)d" % {'el' : elapsed}
+	else :
+		print "First context update" 
 
-	if elapsed > ctx_interval :
+	if elapsed > ctx_interval or last_ctx_updated == 0 :
 		logging.info("it's time for context update")
+		res = subprocess.call("rm ctx", shell=True)
 		res = subprocess.call("wget -nc http://smll.herokuapp.com/device/serial/ctx", shell=True)
 
-		t_new = 0
-		t_now = 0
+		#if os.path.isfile("ctx") :
+	  #	t_new = time.ctime(os.path.getctime("ctx"))
 
-		if os.path.isfile("ctx") :
-			t_new = time.ctime(os.path.getctime("ctx"))
+		#if os.path.isfile("ctx.now") :
+			#t_now = time.ctime(os.path.getctime("ctx.now"))
 
-		if os.path.isfile("ctx.now") :
-			t_now = time.ctime(os.path.getctime("ctx.now"))
-
-		if t_now != t_new  :
-			print "ctx update"
-			print "copy ctx -> ctx.now"
-			subprocess.call("cp -f ctx ctx.now", shell=True)
-			play_index = 0
-			file_download()
-		else :
-			print "no ctx update"
-			time.sleep(1) #wait before checking again if nothing detected
+		#if t_now != t_new  :
+		#print "ctx update"
+		#print "copy ctx -> ctx.now"
+		#subprocess.call("cp -f ctx ctx.now", shell=True)
+		track = 0
+		file_download()
+		#else :
+		#	print "no ctx update"
+		#	time.sleep(1) #wait before checking again if nothing detected
 
 		# update last updated time
 		last_ctx_updated = now 
 
 def file_download() :
-	lines = tuple(open("./ctx.now", 'r'))	
+	lines = tuple(open("./ctx", 'r'))	
 	for line in lines:
 		res = subprocess.call("wget -nc http://smll.herokuapp.com/speech/9/"+line, shell=True)
 
@@ -72,14 +73,17 @@ def process() :
 	ctx_update()
 
 def play() :
-	global play_index
+	global track 
 
 	lines = tuple(open("./ctx", 'r'))	
-	res = subprocess.call("mpg123 " + lines[play_index], shell=True)
 
-	if play_index >= len(lines) :
-		play_index = 0
+	# track reset
+	if track >= len(lines) :
+		track = 0
+	else :
+		track = track + 1 
 	
+	res = subprocess.call("mpg123 " + lines[track], shell=True)
 
 def main() :
 
